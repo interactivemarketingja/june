45280a1f1c7609f33ad9812d3e784785c26487b2
Island Web Consultant <interactivemarketingja@gmail.com>
2025-12-03 03:38:41 +0000

    the logo, give it some padding or margin from the left

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -6,7 +6,7 @@ export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
-        <div className="mr-4 flex">
+        <div className="mr-4 flex md:ml-4">
           <Link href="/" className="mr-6 flex items-center space-x-2">
             <HandHeart className="h-6 w-6" style={{ color: 'hsl(var(--accent-foreground))' }} />
             <span className="font-bold font-headline sm:inline-block">
