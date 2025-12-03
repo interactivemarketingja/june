03b90fe1ea9bb0a944be81228717e5ec72505b27
Island Web Consultant <interactivemarketingja@gmail.com>
2025-12-03 04:18:30 +0000

    remove the hope progress section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Header } from '@/components/landing/header';
 import { Hero } from '@/components/landing/hero';
 import { Bio } from '@/components/landing/bio';
-import { Updates } from '@/components/landing/updates';
 import { Footer } from '@/components/landing/footer';
 
 export default function Home() {
@@ -11,7 +10,6 @@ export default function Home() {
       <main className="flex-1">
         <Hero />
         <Bio />
-        <Updates />
       </main>
       <Footer />
     </div>
