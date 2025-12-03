3d556bbaca1e9ced9eb73a0545c8442429c67331
Island Web Consultant <interactivemarketingja@gmail.com>
2025-12-03 04:32:02 +0000

    footer text can be a little bigger and black color

--- src/components/landing/footer.tsx
+++ src/components/landing/footer.tsx
@@ -5,12 +5,12 @@ export function Footer() {
     <footer className="py-6 md:px-8 md:py-0 bg-primary border-t">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
         <div className="flex items-center gap-2">
-            <HandHeart className="h-5 w-5" style={{ color: 'hsl(var(--accent-foreground))' }} />
-            <p className="text-center text-sm leading-loose text-muted-foreground md:text-left font-headline">
+            <HandHeart className="h-5 w-5 text-foreground" />
+            <p className="text-center text-base leading-loose text-foreground md:text-left font-headline">
               June Wright
             </p>
         </div>
-        <p className="text-center text-sm text-muted-foreground md:text-left">
+        <p className="text-center text-base text-foreground md:text-left">
           © {new Date().getFullYear()} June Wright. All Rights Reserved.
         </p>
       </div>
